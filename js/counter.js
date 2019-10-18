c2170ae47aab5bc3d8c4778292678fedab13212b
BB5014 <bblabriz@bbox.fr>
2019-10-18 19:11:09 +0200

    In form add checkbox, value required

---
+++ js/counter.js
@@ -0,0 +1,19 @@
+
+			$(document).ready(function() {
+				$(".counter1").counterUp({
+					delay: 10,
+					time: 1500
+				});
+				$(".counter2").counterUp({
+					delay: 1500,
+					time: 2500
+				});
+				$(".counter3").counterUp({
+					delay: 2500,
+					time: 3500
+				});
+				$(".counter4").counterUp({
+					delay: 3500,
+					time: 4500
+				});
+			});
